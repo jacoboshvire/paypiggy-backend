bbc27a98a9cbfd70e282d1698da6eab8752ccb43
Jacob Oshevire <ejirojacob2000@gmail.com>
2026-03-15 17:22:30 +0000

    auto commit @Sun Mar 15 17:22:30 GMT 2026

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ const express = require("express");
 const userRoutes = require("./routes/userRoutes");
 const accountRoutes = require("./routes/accountRoutes");
 const transactionRoutes = require("./routes/transactionRoutes");
-const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 app.use(express.json());
 app.use("/api/users", userRoutes);
+app.use("api/account", accountRoutes);
+app.use("api/transaction", transactionRoutes);
 
 app.listen(8080, () => console.log("Running on port 8080"));

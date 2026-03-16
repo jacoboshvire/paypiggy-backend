4e4a1203eb958ff0c8b299b7d6bbd0ab802a83c1
Jacob Oshevire <ejirojacob2000@gmail.com>
2026-03-16 09:45:04 +0000

    auto commit @Mon Mar 16 09:45:04 GMT 2026

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ const mysql = require("mysql2");
 const pool = mysql
   .createPool({
     host: "localhost",
-    user: "root",
+    user: process.env.DB_USER,
     password: "your-password",
     database: "your-database",
   })

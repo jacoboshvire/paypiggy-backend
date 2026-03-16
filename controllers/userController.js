be9029f96669faf45820a13080b82d360882f171
Jacob Oshevire <ejirojacob2000@gmail.com>
2026-03-16 10:12:46 +0000

    auto commit @Mon Mar 16 10:12:46 GMT 2026

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,12 @@
 /** @format */
 
-userController.js;
+const { fetchUsers, insertUser } = require("../models/userModel");
+
+exports.getUsers = async (req, res) => {
+  try {
+    const users = await fetchUsers();
+    res.json({ status: "success", data: users });
+  } catch (err) {
+    res.status(500).json({ status: "error", message: err.message });
+  }
+};

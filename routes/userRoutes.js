445f4316d9cb900bf0cf661086450dfc5f32f5a1
Jacob Oshevire <ejirojacob2000@gmail.com>
2026-03-16 10:19:55 +0000

    auto commit @Mon Mar 16 10:19:55 GMT 2026

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,6 +5,6 @@ const router = express.Router();
 const { getUsers, createUser } = require("../controllers/userController");
 
 router.get("/", getUsers);
-router.post("/", createUser);
+// router.post("/", createUser);
 
 module.exports = router;

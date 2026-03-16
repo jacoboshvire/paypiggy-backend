b84ef6839f94c1e136446f586e01f6b1805414a7
Jacob Oshevire <ejirojacob2000@gmail.com>
2026-03-16 11:56:02 +0000

    auto commit @Mon Mar 16 11:56:02 GMT 2026

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 /** @format */
 
 const express = require("express");
-const cors = require("cors");
+const cors = require("cros");
 
 const userRoutes = require("./routes/userRoutes");
 const accountRoutes = require("./routes/accountRoutes");

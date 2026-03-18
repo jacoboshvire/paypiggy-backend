f4dc02e3bbc2baf3beb7fe211eb024875e387f66
Jacob Oshevire <ejirojacob2000@gmail.com>
2026-03-18 09:42:11 +0000

    auto commit @Wed Mar 18 09:42:11 GMT 2026

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,11 @@
 /** @format */
 
-// const express = require("express");
-// const router = express.Router();
+const express = require("express");
+const router = express.Router();
 
-// module.exports = router;
+const { register, login } = require("../controllers/auth.controller");
+
+router.post("/register", register);
+router.post("/login", login);
+
+module.exports = router;

8c47201b5ffe57e0ae2a5fbda6e8bb4b03372af6
Jacob Oshevire <ejirojacob2000@gmail.com>
2026-03-18 09:38:06 +0000

    auto commit @Wed Mar 18 09:38:06 GMT 2026

--- config/db.js
+++ config/db.js
@@ -12,7 +12,7 @@ const pool = mysql
     host: "localhost",
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: "your-database",
+    database: process.env.DB_DATABASE_NAME,
   })
   .promise();
 

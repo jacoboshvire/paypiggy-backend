8aed0e0296240993d06aac99267b5ade96b78f41
Jacob Oshevire <ejirojacob2000@gmail.com>
2026-03-18 10:28:23 +0000

    auto commit @Wed Mar 18 10:28:23 GMT 2026

--- routes/accountRoutes.js
+++ routes/accountRoutes.js
@@ -2,15 +2,15 @@
 
 const express = require("express");
 const router = express.Router();
-const {
-  getAccount,
-  deleteAccount,
-  postAccount,
-} = require("../controllers/transactionController");
+// const {
+//   getAccount,
+//   deleteAccount,
+//   postAccount,
+// } = require("../controllers/transactionController");
 const { get } = require("./userRoutes");
 
-router.get("/", getAccount);
-router.post("/", postAccount);
-router.delete("/", deleteAccount);
+// router.get("/", getAccount);
+// router.post("/", postAccount);
+// router.delete("/", deleteAccount);
 
 module.exports = router();

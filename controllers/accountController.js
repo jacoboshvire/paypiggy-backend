82bfe5cccb27d4cbaf6b5e0e50be79dab8aa335d
Jacob Oshevire <ejirojacob2000@gmail.com>
2026-03-21 12:56:26 +0000

    auto commit @Sat Mar 21 12:56:26 GMT 2026

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,1 +1,15 @@
 /** @format */
+/** @format */
+
+const express = require("express");
+const router = express.Router();
+const accountController = require("../controllers/accountController");
+const verifyToken = require("../middleware/authMiddleware");
+
+router.post("/", verifyToken, accountController.createAccount);
+router.get("/", verifyToken, accountController.getAllAccounts);
+router.get("/:id", verifyToken, accountController.getAccountById);
+router.put("/:id", verifyToken, accountController.updateAccount);
+router.delete("/:id", verifyToken, accountController.deleteAccount);
+
+module.exports = router;
--- routes/accountRoutes.js
+++ routes/accountRoutes.js
@@ -2,15 +2,13 @@
 
 const express = require("express");
 const router = express.Router();
-// const {
-//   getAccount,
-//   deleteAccount,
-//   postAccount,
-// } = require("../controllers/transactionController");
-// const { get } = require("./userRoutes");
+const accountController = require("../controllers/accountController");
+const verifyToken = require("../middleware/authMiddleware");
 
-// router.get("/", getAccount);
-// router.post("/", postAccount);
-// router.delete("/", deleteAccount);
+router.post("/", verifyToken, accountController.createAccount);
+router.get("/", verifyToken, accountController.getAllAccounts);
+router.get("/:id", verifyToken, accountController.getAccountById);
+router.put("/:id", verifyToken, accountController.updateAccount);
+router.delete("/:id", verifyToken, accountController.deleteAccount);
 
-module.exports = router();
+module.exports = router;

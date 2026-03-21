b90438757b6a822be96f9b28d0a93a1b74775f71
Jacob Oshevire <ejirojacob2000@gmail.com>
2026-03-21 13:05:16 +0000

    auto commit @Sat Mar 21 13:05:16 GMT 2026

--- routes/accountRoutes.js
+++ routes/accountRoutes.js
@@ -4,8 +4,10 @@ const express = require("express");
 const router = express.Router();
 const accountController = require("../controllers/accountController");
 const verifyToken = require("../middleware/auth.middleware");
+const fraudCheck = require("../middleware/fraudMiddleware");
 
-router.post("/", verifyToken, accountController.createAccount);
+// fraudCheck only applies to POST (account creation)
+router.post("/", verifyToken, fraudCheck, accountController.createAccount);
 router.get("/", verifyToken, accountController.getAllAccounts);
 router.get("/:id", verifyToken, accountController.getAccountById);
 router.put("/:id", verifyToken, accountController.updateAccount);

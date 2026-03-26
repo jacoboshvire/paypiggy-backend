0df346bdf58a3d02f7c32bf5039dfd488574489e
Jacob Oshevire <ejirojacob2000@gmail.com>
2026-03-26 16:09:07 +0000

    auto commit @Thu Mar 26 16:09:07 GMT 2026

--- routes/accountRoutes.js
+++ routes/accountRoutes.js
@@ -2,10 +2,10 @@
 
 const express = require("express");
 const router = express.Router();
-const accountController = require("../controllers/accountController");
-const verifyToken = require("../middleware/auth.middleware");
-const fraudCheck = require("../middleware/fraudMiddleware");
-const ukOnlyGeoCheck = require("../middleware/geoMiddleware");
+const { accountController } = require("../controllers/accountController");
+const { verifyToken } = require("../middleware/auth.middleware");
+const { fraudCheck } = require("../middleware/fraudMiddleware");
+const { ukOnlyGeoCheck } = require("../middleware/geoMiddleware");
 
 router.post(
   "/",

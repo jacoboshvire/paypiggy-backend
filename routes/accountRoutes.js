300c38f3447f50e9176bb243bbf6b982dce3e8f9
Jacob Oshevire <ejirojacob2000@gmail.com>
2026-03-26 16:21:16 +0000

    auto commit @Thu Mar 26 16:21:16 GMT 2026

--- routes/accountRoutes.js
+++ routes/accountRoutes.js
@@ -2,22 +2,15 @@
 
 const express = require("express");
 const router = express.Router();
-
-// ----------------------
 // MIDDLEWARE IMPORTS
-// ----------------------
 const { verifyToken } = require("../middleware/auth.middleware");
 const { fraudCheck } = require("../middleware/fraudMiddleware");
 const { ukOnlyGeoCheck } = require("../middleware/geoMiddleware");
 
-// ----------------------
 // CONTROLLERS
-// ----------------------
 const accountController = require("../controllers/accountController");
 
-// ----------------------
 // ROUTES
-// ----------------------
 
 // Create a new account
 router.post(
@@ -25,41 +18,17 @@ router.post(
   verifyToken, // JWT auth
   ukOnlyGeoCheck, // Geo-location check (UK only)
   fraudCheck, // Fraud detection
-  async (req, res, next) => {
-    try {
-      await accountController.createAccount(req, res);
-    } catch (err) {
-      next(err); // async-safe
-    }
-  },
+  accountController.createAccount,
 );
 
 // Get all accounts
-router.get("/", verifyToken, ukOnlyGeoCheck, async (req, res, next) => {
-  try {
-    await accountController.getAllAccounts(req, res);
-  } catch (err) {
-    next(err);
-  }
-});
+router.get("/", verifyToken, ukOnlyGeoCheck, accountController);
 
 // Get a single account by ID
-router.get("/:id", verifyToken, ukOnlyGeoCheck, async (req, res, next) => {
-  try {
-    await accountController.getAccountById(req, res);
-  } catch (err) {
-    next(err);
-  }
-});
+router.get("/:id", verifyToken, ukOnlyGeoCheck, accountController);
 
 // Update account
-router.put("/:id", verifyToken, ukOnlyGeoCheck, async (req, res, next) => {
-  try {
-    await accountController.updateAccount(req, res);
-  } catch (err) {
-    next(err);
-  }
-});
+router.put("/:id", verifyToken, ukOnlyGeoCheck, accountController);
 
 // Delete account
 router.delete("/:id", verifyToken, ukOnlyGeoCheck, async (req, res, next) => {

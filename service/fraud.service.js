787cf095efe81a30bc5f26670923ed86e4148033
Jacob Oshevire <ejirojacob2000@gmail.com>
2026-03-30 14:28:08 +0100

    auto commit @Mon Mar 30 14:28:08 BST 2026

--- service/fraud.service.js
+++ service/fraud.service.js
@@ -3,7 +3,25 @@
 const rateLimit = require("express-rate-limit");
 
 exports.transferLimiter = rateLimit({
-  windowMs: 60 * 1000, // 1 min
-  max: 5, // max 5 transfers per minute
+  windowMs: 60 * 1000,
+  max: 5,
   message: "Too many requests",
 });
+
+exports.calculateRisk = async ({ userId, amount }) => {
+  const reasons = [];
+  let risk = 0;
+
+  // Large amount adds risk
+  if (amount > 5000) {
+    risk += 40;
+    reasons.push("Large transaction amount");
+  }
+
+  if (amount > 8000) {
+    risk += 30;
+    reasons.push("Very large transaction amount");
+  }
+
+  return { risk, reasons };
+};

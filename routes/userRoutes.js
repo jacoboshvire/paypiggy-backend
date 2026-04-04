3f5dacb9d74b17c5ce8dc84b3e3d4a86c0013f68
Jacob Oshevire <ejirojacob2000@gmail.com>
2026-04-04 11:20:48 +0100

    auto commit @Sat Apr  4 11:20:48 BST 2026

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,7 +4,7 @@ const express = require("express");
 const router = express.Router();
 const {
   getUsers,
-  createUser,
+  getUserById,
   updateUser,
 } = require("../controllers/userController");
 

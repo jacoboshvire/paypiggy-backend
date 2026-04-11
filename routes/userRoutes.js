375f076fdb37786c1b9b1d58b09c0c5220f47371
Jacob Oshevire <ejirojacob2000@gmail.com>
2026-04-11 14:44:31 +0100

    auto commit @Sat Apr 11 14:44:31 BST 2026

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,10 +5,16 @@ const {
   getUsers,
   getUserById,
   updateUser,
+  forgotPassword,
+  verifyForgotOtp,
+  resetPassword,
 } = require("../controllers/userController");
 
 router.get("/", getUsers);
 router.get("/:id", getUserById);
 router.put("/:id", updateUser);
+router.post("/forgot-password", forgotPassword);
+router.post("/verify-forgot-otp", verifyForgotOtp);
+router.post("/reset-password", resetPassword);
 
 module.exports = router;

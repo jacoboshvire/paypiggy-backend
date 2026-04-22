e2b6254c2c68460a21b284f328f122a9d281f33d
Jacob Oshevire <ejirojacob2000@gmail.com>
2026-04-22 09:44:18 +0100

    auto commit @Wed Apr 22 09:44:18 BST 2026

--- app.js
+++ app.js
@@ -12,7 +12,21 @@ const settingsRoutes = require("./routes/settingsRoutes");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "https://your-frontend.vercel.app",
+      "http://localhost:3000",
+      "http://localhost:3001",
+    ],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
+
+app.options("*", cors());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 

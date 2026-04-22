06f680a8cca698586e800c3c994ecd4e6bd8cdf9
Jacob Oshevire <ejirojacob2000@gmail.com>
2026-04-22 09:54:56 +0100

    auto commit @Wed Apr 22 09:54:56 BST 2026

--- server.js
+++ server.js
@@ -2,13 +2,11 @@
 
 require("dotenv").config();
 const app = require("./app");
-const cors = require("cors");
-
-app.use(cors());
 
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
 module.exports = app;

c3e8bb96b90a0bf27becf6b7b4eba6e6dc583df3
Jacob Oshevire <ejirojacob2000@gmail.com>
2026-04-25 22:00:51 +0100

    auto commit @Sat Apr 25 22:00:51 BST 2026

--- middleware/geoMiddleware.js
+++ middleware/geoMiddleware.js
@@ -1,7 +1,21 @@
 /** @format */
 
+const { Reader } = require("@maxmind/geoip2-node");
+const path = require("path");
+const fs = require("fs");
+
+const DB_PATH = path.join(__dirname, "../geoip/GeoLite2-Country.mmdb");
+const WHITELISTED_IPS = ["127.0.0.1", "::1", "::ffff:127.0.0.1"];
+
+const getClientIP = (req) => {
+  const forwarded = req.headers["x-forwarded-for"];
+  if (forwarded) {
+    return forwarded.split(",")[0].trim();
+  }
+  return req.socket.remoteAddress;
+};
+
 const ukOnlyGeoCheck = async (req, res, next) => {
-  // Use Vercel's built-in geo headers
   const country = req.headers["x-vercel-ip-country"];
 
   if (country && country !== "GB") {
@@ -12,14 +26,12 @@ const ukOnlyGeoCheck = async (req, res, next) => {
     });
   }
 
-  // Fall back to GeoIP for non-Vercel environments
   const ip = getClientIP(req);
   if (WHITELISTED_IPS.includes(ip)) {
     return next();
   }
 
   try {
-    const fs = require("fs");
     if (!fs.existsSync(DB_PATH)) {
       return next();
     }
@@ -43,4 +55,4 @@ const ukOnlyGeoCheck = async (req, res, next) => {
   }
 };
 
-export default ukOnlyGeoCheck;
+module.exports = { ukOnlyGeoCheck };

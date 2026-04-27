6159071210be8d733bc354eca7417b8fa843a13f
Jacob Oshevire <ejirojacob2000@gmail.com>
2026-04-27 13:33:41 +0100

    auto commit @Mon Apr 27 13:33:41 BST 2026

--- middleware/geoMiddleware.js
+++ middleware/geoMiddleware.js
@@ -40,7 +40,7 @@ const ukOnlyGeoCheck = async (req, res, next) => {
     const result = reader.country(ip);
     const countryCode = result.country?.isoCode;
 
-    if (countryCode !== "US") {
+    if (countryCode !== "UK") {
       return res.status(403).json({
         error: "Access denied",
         reason: "This service is only available in the United Kingdom",
